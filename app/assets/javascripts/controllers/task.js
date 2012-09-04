7f8cf8abd830f9279b4c59c547a75c0211c43386
swapnilp04 <swapnil.patil04@gmail.com>
2012-09-04 19:07:52 +0530

    committed

--- app/assets/javascripts/controllers/task.js
+++ app/assets/javascripts/controllers/task.js
@@ -39,8 +39,7 @@ function tasksCtrl($scope, $http, $location) {
     $scope.edit_tasks = false;
     $scope.show_subtask = true;
     $scope.mytasks = true;
-    //$scope.todayDate = new Date();
-  
+
     
     $scope.fetchTasks = function() {
 	$http.get('tasks').
